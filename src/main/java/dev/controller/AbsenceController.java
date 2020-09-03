3f62acc244cf7a2033655ddbeee24bc1a36b3048
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-09-03 15:57:02 +0200

    code déplacé dans le service et non le controller

--- src/main/java/dev/controller/AbsenceController.java
+++ src/main/java/dev/controller/AbsenceController.java
@@ -4,12 +4,10 @@
 package dev.controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import javax.validation.Valid;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,8 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.controller.vm.AbsencePostVM;
 import dev.controller.vm.AbsenceVM;
-import dev.domain.Collegue;
-import dev.repository.CollegueRepo;
 import dev.service.AbsenceService;
 
 /**
@@ -36,46 +32,32 @@ import dev.service.AbsenceService;
 public class AbsenceController {
 
 	private AbsenceService absenceService;
-	private CollegueRepo collegueRepo;
 
 	/**
 	 * Constructeur
 	 * 
 	 * @param absenceRepo
-	 * @param collegueRepo
 	 */
-	public AbsenceController(AbsenceService absenceRepo, CollegueRepo collegueRepo) {
+	public AbsenceController(AbsenceService absenceRepo) {
 		super();
 		this.absenceService = absenceRepo;
-		this.collegueRepo = collegueRepo;
-	}
-
-	public Optional<Collegue> getColConnecte() {
-		String email = SecurityContextHolder.getContext().getAuthentication().getName();
-		return this.collegueRepo.findByEmail(email);
 	}
 
 	@GetMapping
 	public List<AbsenceVM> getListAbsence() {
-
-		Optional<Collegue> col = this.getColConnecte();
-		return this.absenceService.findAbsences(col.get());
+		return this.absenceService.findAbsences();	
 	}
-
+	
 	@DeleteMapping("{uuid}")
 	public void supprAbsence(@PathVariable UUID uuid) {
-
-		Optional<Collegue> col = this.getColConnecte();
-		this.absenceService.deleteAbs(uuid, col.get());
+		this.absenceService.deleteAbs(uuid);
 	}
 
 	@PostMapping
 	public AbsencePostVM newAbsence(@RequestBody @Valid AbsencePostVM newAbs, BindingResult res) {
 		if(res.hasErrors()) {
-			System.out.println("error");
-		}
-		Collegue col = getColConnecte().get();
-		
-		 return this.absenceService.saveAbs(newAbs, col);
+			throw new RuntimeException("Données incorrects pour post absence");
+		}	
+		return this.absenceService.saveAbs(newAbs);
 	}
 }
--- src/main/java/dev/service/AbsenceService.java
+++ src/main/java/dev/service/AbsenceService.java
@@ -5,6 +5,7 @@ package dev.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
@@ -17,6 +18,7 @@ import dev.domain.Absence;
 import dev.domain.Collegue;
 import dev.domain.enumerations.Status;
 import dev.repository.AbsenceRepo;
+import dev.repository.CollegueRepo;
 
 /**
  * @author robin
@@ -24,34 +26,53 @@ import dev.repository.AbsenceRepo;
  */
 @Service
 @Transactional
-public class AbsenceService {
+public class AbsenceService extends LogService{
 
 	private AbsenceRepo absenceRepo;
 
-	public AbsenceService(AbsenceRepo absenceRepo) {
-		super();
+	public AbsenceService(AbsenceRepo absenceRepo, CollegueRepo collegueRepo) {
+		super(collegueRepo);
 		this.absenceRepo = absenceRepo;
 	}
 
-	public List<AbsenceVM> findAbsences(Collegue col) {
-		List<Absence> tmp = this.absenceRepo.findAbsences(col);
 
-		List<AbsenceVM> res = new ArrayList<>();
+	public List<AbsenceVM> findAbsences() {
 
-		for (Absence a : tmp) {
-			res.add(new AbsenceVM(a.getUuid(), a.getDateDebut(), a.getDateFin(), a.getType(), a.getStatus(), a.getMotif()));
+		Optional<Collegue> col = this.getColConnecte();
+
+		if (col.isPresent()) {
+
+			List<Absence> tmp = this.absenceRepo.findAbsences(col.get());
+			List<AbsenceVM> res = new ArrayList<>();
+
+			for (Absence a : tmp) {
+				res.add(new AbsenceVM(a.getUuid(), a.getDateDebut(), a.getDateFin(), a.getType(), a.getStatus(),
+						a.getMotif()));
+			}
+			return res;
 		}
-		return res;
-	}
-	
-	public void deleteAbs(UUID uuid, Collegue col) {
-		this.absenceRepo.deleteAbs(uuid, col);
-	}
-	
-	public AbsencePostVM saveAbs(AbsencePostVM a, Collegue col) {
-		Absence tmp = absenceRepo.save(new Absence(a.getDateDebut(), a.getDateFin(), a.getType(), Status.STATUS_INITIAL, a.getMotif(), col));
-		AbsencePostVM abspost = new AbsencePostVM(tmp.getDateDebut(), tmp.getDateFin(), tmp.getType(), tmp.getMotif());
-		return abspost;
+		throw new RuntimeException("Error find absence");
 	}
 
+	public void deleteAbs(UUID uuid) {
+		Optional<Collegue> col = this.getColConnecte();
+		if (col.isPresent()) {
+			this.absenceRepo.deleteAbs(uuid, col.get());
+		} else {
+			throw new RuntimeException("Error delete absence");
+		}
+	}
+
+	public AbsencePostVM saveAbs(AbsencePostVM a) {
+		Optional<Collegue> col = getColConnecte();
+		if (col.isPresent()) {
+
+			Absence tmp = absenceRepo.save(new Absence(a.getDateDebut(), a.getDateFin(), a.getType(),
+					Status.STATUS_INITIAL, a.getMotif(), col.get()));
+			AbsencePostVM abspost = new AbsencePostVM(tmp.getDateDebut(), tmp.getDateFin(), tmp.getType(),
+					tmp.getMotif());
+			return abspost;
+		}
+		throw new RuntimeException("Error save absence");
+	}
 }
