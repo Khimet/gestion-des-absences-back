81783c1e70ebb9b8e1e532b16ed8de0cd1e6f4cc
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-09-09 02:18:51 +0200

    demande rejetee = +1 compteur

--- src/main/java/dev/repository/CollegueRepo.java
+++ src/main/java/dev/repository/CollegueRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import dev.domain.Collegue;
@@ -15,4 +16,12 @@ public interface CollegueRepo extends JpaRepository<Collegue, Long> {
     
     @Query("select c from Collegue c where c.departement = ?1")
 	List<Collegue> findCollegueDepartement(Departement depart);
+
+    @Modifying
+    @Query("update Collegue c set c.nbRtt = ?1 where c.id = ?2")
+	void setCompteursRttPlus1(int nb, Long id);
+    
+    @Modifying
+    @Query("update Collegue c set c.nbCongesPayes = ?1 where c.id = ?2")
+    void setCompteursCongePlus1(int nb, Long id);
 }
